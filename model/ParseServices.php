edbcd75a06747d1f8cd54b355a4bf348300f6fa1
Alkior <39372901+Alkior@users.noreply.github.com>
2022-02-10 20:42:48 +0400

    Update ParseServices.php

--- model/ParseServices.php
+++ model/ParseServices.php
@@ -17,8 +17,8 @@ public $parameter;
         if(!empty($getSearch)){$post = substr($post, 0, 200); $this->parameter = '/'. str_replace(' ', '&', $getSearch);}
         else{$this->parameter = '';}
         $this->HttpClient = new HttpClient();
-        $name = iconv("windows-1251", "UTF-8", 'HTTP_service');
-        $url = "http://".$name.":CUZmsch5687@10.20.3.6/hospital_pr1/hs/BaseInfo/Get".$this->parameter;    
+        $name = iconv("windows-1251", "UTF-8", 'HTTPS_service');
+        $url = "http://".$name.":CUZmsch5687@10.20.3.6/hospital/hs/BaseInfo/Get".$this->parameter;    
         $this->cUrl = \Bitrix\Main\Web\Json::decode($this->HttpClient->get($url));        
     }
     
